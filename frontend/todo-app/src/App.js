d35a8b1c2ca6fabae93bb576e490f1996d8e57c5
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2019-05-05 17:40:37 +0200

    Added class components and function components
    Added class components and function components

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -7,9 +7,52 @@ class App extends Component {
     return (
       <div className="App">
         My Todo Application
+        <FirstComponent />
+        <SecondComponent />
+        <ThirdComponent />
+        <FourthComponent />
       </div>
     );
   }
 }
+
+// Class Component, need to extend component and override Render-method
+class FirstComponent extends Component {
+  render() {
+    return (
+      <div className="FirstComponent">
+        First Component
+      </div>
+    );
+  }
+}
+
+class SecondComponent extends Component {
+  render() {
+    return (
+      <div className="SecondComponent">
+        Second Component
+      </div>
+    );
+  }
+}
+
+// Function component
+function ThirdComponent() {
+  return (
+    <div className="ThirdComponent">
+      Third Component
+    </div>
+  );
+}
+
+function FourthComponent() {
+  return (
+    <div className="FourthComponent">
+      Fourth Component
+    </div>
+  );
+}
+
 
 export default App;
