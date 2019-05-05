f41823ed9148f1ffdaba14d822ddf6249cef3b66
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2019-05-05 19:41:34 +0200

    Created component with State
    Created component with State and added counter with + 1

--- frontend/todo-app/src/components/counter/Counter.jsx
+++ frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './Counter.css';
 
-function Counter() {
-    return (
-        <div className="counter">
-           <button>+1</button>
-           <span className="count">0</span>
-        </div>
-    )
+    /*  Give state to the class  */
+    // Define the initial state in a constructor, always call super-method
+    // Bind methods to context that need to change the state
+    // Update the state in methods via setSate
+
+// When calling a function within JSX don't use the parentheses after the method, just the reference.
+// When calling a local function within a class-component then put 'this' before the method.
+class Counter extends Component {
+
+    // creating constructor
+    constructor(){
+        // always call super in constructor
+        super(); 
+        // setting state
+        this.state = {
+            counter : 0
+        }
+        
+        // binding the method to the component
+        this.increment = this.increment.bind(this);
+    }
+
+    render(){
+        return (
+            <div className="counter">
+               <button onClick={this.increment}>+1</button>
+               <span className="count">{this.state.counter}</span>
+            </div>
+        )
+    }
+
+    increment() {
+        // Always call setState when changing state
+        this.setState({
+          counter: this.state.counter + 1   
+        });
+    }
 }
 
 export default Counter;
