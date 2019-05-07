d8f3d4fd25f6a3fa1c35d45b319b45bc467435a4
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2019-05-07 17:48:10 +0200

    Added event listeners to the form
    Added event listeners to the form

--- frontend/todo-app/src/components/todo/ToDoApp.jsx
+++ frontend/todo-app/src/components/todo/ToDoApp.jsx
@@ -10,15 +10,45 @@ class ToDoApp extends Component {
     }
 }
 
+/**
+ * This component handles the login.
+ * To change the input in forms event listeners must be used to change the form or else the value would be read only. First the state must be initialized.
+ **/
 class LoginComponent extends Component {
+
+    // Constructor for default values of the form
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+
+        this.usernameChange = this.usernameChange.bind(this);
+        this.passwordChange = this.passwordChange.bind(this);
+    }
+
     render() {
         return (
             <div>
-               Username:  <input type="text" name="username" /> 
-               Password:  <input type="password" name="password" />
+               Username:  <input type="text" name="username" value={this.state.username} onChange={this.usernameChange} /> 
+               Password:  <input type="password" name="password" value={this.state.password} onChange={this.passwordChange}/>
                <button>Login</button>
             </div>
         )
+    }
+
+    // Event handler for changing value
+    usernameChange(event) {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    passwordChange(event) {
+        this.setState({
+            password: event.target.value
+        })
     }
 }
 
