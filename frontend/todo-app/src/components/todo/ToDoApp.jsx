5de1f8eb32e1188f52818904ef3dcbd817ecbd06
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2019-05-11 15:42:53 +0200

    Created authentication service
    Created authentication service for logging in and out.

--- frontend/todo-app/src/components/todo/ToDoApp.jsx
+++ frontend/todo-app/src/components/todo/ToDoApp.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
+import AuthenticationService from './AuthenticationService.js';
 
 import './ToDoApp.css';
 
@@ -7,20 +8,18 @@ class ToDoApp extends Component {
     render() {
         return (
             <div className="ToDoApp">
-                <Router>
-                    <>
-                        <HeaderComponent />
-                        <Switch>
-                            <Route path="/" exact component={LoginComponent} />
-                            <Route path="/login" component={LoginComponent} />
-                            <Route path="/welcome/:name" component={WelcomeComponent} />
-                            <Route path="/todo" component={ListTodosComponent} />
-                            <Route path="/logout" component={LogoutComponent} />
-                            <Route component={ErrorComponent} />
-                        </Switch>
-                        <FooterComponent />
-                    </>
-                </Router>
+                    <Router>
+                            <HeaderComponent />
+                            <Switch>
+                                <Route path="/" exact component={LoginComponent} />
+                                <Route path="/login" component={LoginComponent} />
+                                <Route path="/welcome" component={WelcomeComponent} />
+                                <Route path="/todo" component={ListTodosComponent} />
+                                <Route path="/logout" component={LogoutComponent} />
+                                <Route component={ErrorComponent} />
+                            </Switch>
+                            <FooterComponent />
+                    </Router>
             </div>
         )
     }
@@ -29,17 +28,19 @@ class ToDoApp extends Component {
 // Component for creating Header
 class HeaderComponent extends Component {
     render(){
+        const isUserLoggedIn = AuthenticationService.isUserLoggedIn();
+
         return (
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
                     <div><a href="/" className="navbar-brand">Logo</a></div>
                     <ul className="navbar-nav ">
-                        <li><Link className="nav-link" to="/welcome/Ronald">Home</Link></li>
-                        <li><Link className="nav-link" to="/todo">Todos</Link></li>
+                        {isUserLoggedIn && <li><Link className="nav-link" to="/welcome">Home</Link></li>}
+                        {isUserLoggedIn && <li><Link className="nav-link" to="/todo">Todos</Link></li>}
                     </ul>
                     <ul className="navbar-nav navbar-collapse justify-content-end">
-                        <li><Link className="nav-link" to="/login">Login</Link></li>
-                        <li><Link className="nav-link" to="/logout">Logout</Link></li>
+                        {!isUserLoggedIn && <li><Link className="nav-link" to="/login">Login</Link></li>}
+                        {isUserLoggedIn && <li><Link className="nav-link" to="/logout" onClick={AuthenticationService.logout}>Logout</Link></li>}
                     </ul>
                 </nav>
             </header>
@@ -66,7 +67,7 @@ class WelcomeComponent extends Component {
             <>
                 <h1>Welcome!</h1>
                 <div className="container">
-                   <p>Welcome {this.props.match.params.name}! You can manage your todos <Link to="/todo">here</Link>.</p>
+                   <p>Welcome {sessionStorage.getItem('authenticatedUser')}! You can manage your todos <Link to="/todo">here</Link>.</p>
                 </div>
             </>
         )
@@ -105,7 +106,7 @@ class ListTodosComponent extends Component {
                             {
                                 this.state.todo.map (
                                     todo => 
-                                        <tr>                          
+                                        <tr key={todo.id}>                          
                                             <td>{todo.description}</td>
                                             <td>{todo.targetDate.toString()}</td>
                                             <td>{todo.done.toString()}</td>
@@ -168,8 +169,10 @@ class LoginComponent extends Component {
 
     login(){
         if(this.state.username === 'ronald' && this.state.password === "password" ){
+            // use authentication service to save user in sessionStorage
+            AuthenticationService.registerSuccessfulLogin(this.state.username, this.state.password);
             // redirect to welcome page with path param. Need to use ticks when passing variable
-            this.props.history.push(`/welcome/${this.state.username}`)
+            this.props.history.push("/welcome");
         } else {
             this.setState({
                 isLoginFailed: true
@@ -178,8 +181,8 @@ class LoginComponent extends Component {
     }
 }
 
-// Component for loggin out
-class LogoutComponent extends Component {
+// Component for logging out
+class LogoutComponent extends Component {   
     render() {
         return (
             <div>
