df4ef00dbaaf5561ff9139b8942b338a8dfafc53
Ronald Visser <ronald.visser@ndcmediagroep.nl>
2019-05-12 16:02:32 +0200

    Added PathVariable to Hello World Controller
    Added PathVariable to Hello World Controller

--- backend/todo-rest-api/src/main/java/nl/thuis/webservices/todorestapi/HelloWorldController.java
+++ backend/todo-rest-api/src/main/java/nl/thuis/webservices/todorestapi/HelloWorldController.java
@@ -1,6 +1,7 @@
 package nl.thuis.webservices.todorestapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world-bean")
 	public HelloWorld HelloWorldBean() {
 		return new HelloWorld("Hello World");
+	}
+	
+	@GetMapping(path="/hello-world/name/{name}")
+	public HelloWorld HelloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorld(String.format("Hello World, %s", name));
 	}
 }
